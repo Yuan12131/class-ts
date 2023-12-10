ad778eac975e3a4b08f271fc360b07b6ac6e2738
leeyuan <dbdks32123@gmail.com>
2023-12-10 17:44:58 +0900

    repeated 예제 2 compile 한 js

---
+++ class-ts-repeated2-functional.js
@@ -0,0 +1,37 @@
+// 사칙연산 함수 정의
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("0으로 나눌 수 없음");
+    }
+    return a / b;
+}
+// 연산 결과를 출력하는 함수
+/**
+ * @param a number
+ * @param b number
+ * @returns void
+ * @description 사칙연산 결과 출력
+ *
+ * 함수 리턴을 정적으로 명시해주는 typescript의 문법 중에 void라는 단어는 직역하면 회피하다는 뜻이며
+ * 함수의 리턴값이 없다는 것을 명시적으로 표현
+ */
+function printOperations(a, b) {
+    var results = [
+        { operation: '덧셈', result: add(a, b) },
+        { operation: '뺄셈', result: subtract(a, b) },
+        { operation: '곱셈', result: multiply(a, b) },
+        { operation: '나눗셈', result: divide(a, b) }
+    ];
+    console.table(results);
+}
+// 함수 사용 예
+printOperations(10, 5); // 10과 5를 사용한 사칙 연산 결과 출력

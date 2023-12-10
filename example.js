236fa47a30830dec1f7c6a5bc98d9058e084be9a
leeyuan <dbdks32123@gmail.com>
2023-12-10 20:22:18 +0900

    ts DOM 조작 예제 compile

--- example.js
+++ example.js
@@ -1,3 +1,7 @@
 function aTwo(selectElement, text) {
     selectElement.innerHTML = "<h1>".concat(text, "</h1>");
 }
+document.addEventListener("DOMContentLoaded", function () {
+    var root = document.getElementById("root");
+    aTwo(root, "Hello");
+});

fd493b1eb37d63cfe36251808fa1cab4ce18e245
leeyuan <dbdks32123@gmail.com>
2023-12-10 20:21:17 +0900

    ts DOM 조작 예제1

--- example.ts
+++ example.ts
@@ -1,3 +1,8 @@
 function aTwo(selectElement: HTMLElement, text:string):void {
   selectElement.innerHTML = `<h1>${text}</h1>`
   }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    aTwo(root, "HH");
+  });

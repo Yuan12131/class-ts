790ad33affa418f8f62d99c198504eb67f571cb7
leeyuan <dbdks32123@gmail.com>
2023-12-12 20:20:24 +0900

    repeated_prac.ts compile

---
+++ repeated_prac.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loadFunction = void 0;
+function loadFunction(name) {
+    console.log("".concat(name, "\uD568\uC218\uAC00 \uB85C\uB4DC\uB418\uC5C8\uC2B5\uB2C8\uB2E4."));
+    return "loadFunction";
+}
+exports.loadFunction = loadFunction;

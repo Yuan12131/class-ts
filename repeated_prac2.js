72cf1643351bd6627d884a11ad64ff839c7dd018
leeyuan <dbdks32123@gmail.com>
2023-12-12 20:20:34 +0900

    repeated_prac2.ts compile

---
+++ repeated_prac2.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.functionA = void 0;
+var repeated_prac_js_1 = require("./repeated_prac.js");
+function functionA() {
+    (0, repeated_prac_js_1.loadFunction)('functionA');
+    return "functionA";
+}
+exports.functionA = functionA;

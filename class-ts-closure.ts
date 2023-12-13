88b9cb3e8366f86c29ec8d927b94404c6bb7a086
leeyuan <dbdks32123@gmail.com>
2023-12-13 20:01:03 +0900

    class-closure

--- class-ts-closure.ts
+++ class-ts-closure.ts
@@ -30,3 +30,14 @@ let MemberDataWithoutClosure: MemberData = {
   cart: ['item3', 'item4']
 };
 
+/**
+ * 위의 객체는 클로저를 사용하지 않았기 때문에 membeDataWithoutClosure.id 등에 직접 접근 가능
+ * 개발할 때는 접근하는 것이 편리하지만 유지보수를 하기에는 위험
+ * 객체지향 프로그래밍에서는 은닉화 작업을 통해 직접 접근 차단
+ * 하지만 함수형 프로그래밍에서는 일반적인 방식으로 은닉화 할 수 없기 때문에
+ * 클로저 패턴, 위 예제에서 meberDataWithClosure 함수를 통해 은닉화 구현
+ */
+
+// 클로저를 사용하는 예제
+let Member1WithClosure = MemberDataWithClosure('user1', 'pass1', 'address1', ['item1', 'item2']);
+console.log(Member1WithClosure()); // 클로저를 통한 데이터 접근

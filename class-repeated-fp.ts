29862ead6d520f87ddfe42478dbfb5471a289c9d
leeyuan <dbdks32123@gmail.com>
2023-12-14 17:46:31 +0900

    class-fp-factory func return obj-method

--- class-repeated-fp.ts
+++ class-repeated-fp.ts
@@ -3,3 +3,12 @@ function sayHelloMinam() {
   return "hello kongukjae";
 }
 
+// chunam의 기능을 팩토리 함수로 구현
+function createChunam() {
+  // 리턴이 객체
+  return {
+    sayHello: function() {
+      return "bye kongukjae";
+    }
+  };
+}

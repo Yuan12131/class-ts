fbc13c9a3131b7ee598b6d8b8ee0e7b5f649df2a
leeyuan <dbdks32123@gmail.com>
2023-12-14 18:01:42 +0900

    class-fp-ts-class instance

--- class-repeated-fp.ts
+++ class-repeated-fp.ts
@@ -5,3 +5,12 @@ class Minam {
 }
 
 console.log(Minam.sayHello());
+
+class Chunam {
+  sayHello(): string {
+    return "bye kongukjae";
+  }
+}
+
+const chunamInstance = new Chunam();
+console.log(chunamInstance.sayHello());

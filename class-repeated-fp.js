bedf049ce23bb3824485e90d6e2473cb4cb0fbcd
leeyuan <dbdks32123@gmail.com>
2023-12-14 17:55:08 +0900

    class-fp-js-class instance

--- class-repeated-fp.js
+++ class-repeated-fp.js
@@ -5,3 +5,16 @@ class Minam {
 }
 // Minam 클래스의 static 메서드 사용 예시
 console.log(Minam.sayHello());
+
+class Chunam {
+  sayHello() {
+    return "bye kongukjae"
+  }
+}
+
+// chunam 클래스의 인스턴스 생성 및 메서드 사용예시
+// new 라는 키워드를 통해서 chunamInstance(객체)를 생성
+// 하지만, 위 Minam 클래스의 sayHello는 new 키워드가 없어도(인스턴스 생성이 필요 없어도) 사용 가능
+// 일반적인 인스턴스 생성은 다분히 동적인 방식
+const chunamInstance = new Chunam();
+console.log(chunamInstance.sayHello());

40ca2465103e8d585c9691c3cd9ecf63088be62c
leeyuan <dbdks32123@gmail.com>
2023-12-15 17:54:45 +0900

    class-15-fetchProducts func

--- class-training15.ts
+++ class-training15.ts
@@ -1,2 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
+const fetchProducts = async () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve ([
+        { id: 1, title: '김치', price: '5000원' },
+        { id: 2, title: '라면', price: '3000원' },
+        { id: 1, title: '삼겹살', price: '15000원' },
+      ]);
+    }, 500)
+  })
+}

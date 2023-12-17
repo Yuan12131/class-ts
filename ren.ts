d7d318472229f6f8a1f29fd8393dd1ade936a70d
leeyuan <dbdks32123@gmail.com>
2023-12-17 18:36:33 +0900

    ts snippchat - ren 예제

--- ren.ts
+++ ren.ts
@@ -1,7 +1,16 @@
-// render 메서드는 React 클래스 컴포넌트에서 반드시 정의해야 하는 메서드: 컴포넌트의 UI를 반환
-// React 엔진에 의해 자동으로 호출되며, 컴포넌트의 렌더링 결과물을 생성
-public render() {
-  return (
-    
-  );
+
+import * as React from 'react';
+
+class MyComponent extends React.Component {
+  // render 메서드는 React 클래스 컴포넌트에서 반드시 정의해야 하는 메서드: 컴포넌트의 UI를 반환
+  // React 엔진에 의해 자동으로 호출되며, 컴포넌트의 렌더링 결과물을 생성
+  // render 메서드 오버라이딩
+  public render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>This is a simple React component.</p>
+      </div>
+    );
+  }
 }

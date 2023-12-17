0651d0aea68cace60e5e6662c7ae2d214e4fc3ef
leeyuan <dbdks32123@gmail.com>
2023-12-17 18:33:31 +0900

    ts snippchat - cdm 예제

--- cdm.ts
+++ cdm.ts
@@ -1,3 +1,35 @@
-componentDidMount() {
-  
+import * as React from 'react';
+
+export interface IAppProps {
 }
+
+export interface IAppState {
+  counter: number;
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  // componentDidMount 메서드를 오버라이딩
+  // 이 메서드는 컴포넌트가 마운트된 후에 호출
+  componentDidMount() {
+    console.log('Component is mounted!');
+    // 여기에서 비동기 작업이나 초기 데이터 로딩 등을 수행
+  }
+
+  public render() {
+    return (
+      <div>
+        <p>Counter: {this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+export default App;

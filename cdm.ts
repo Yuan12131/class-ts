ad2c42baf5f7c608fa32d9b79b3477b04352a8d7
leeyuan <dbdks32123@gmail.com>
2023-12-17 18:33:57 +0900

    ts snippchat - cdm 오버라이딩

--- cdm.ts
+++ cdm.ts
@@ -17,6 +17,8 @@ class App extends React.Component<IAppProps, IAppState> {
   }
 
   // componentDidMount 메서드를 오버라이딩
+  // 오버라이딩(Overriding) : 객체 지향 프로그래밍에서 부모 클래스의 메서드를 자식 클래스에서 재정의
+  // -> 자식 클래스에서 부모 클래스에 이미 정의된 메서드를 같은 이름의 메서드로 다시 정의하고 사용하는 것을 의미
   // 이 메서드는 컴포넌트가 마운트된 후에 호출
   componentDidMount() {
     console.log('Component is mounted!');
